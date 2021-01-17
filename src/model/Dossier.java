1c0b9ad6e8c9c2732dca58c7d1ac5ff8ce582d7f
Mohamed OUFRAd <oufrad00@gmail.com>
2021-01-17 11:28:41 +0100

    Dossier & RDV model

--- src/model/Dossier.java
+++ src/model/Dossier.java
@@ -1,21 +1,74 @@
 package model;
 
+import java.sql.Date;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
 public class Dossier {
 	
 /*--- Properties ---*/
-	static long num=0;
+	
+	private static long numDossier=0;
+	private Date dateCreationDossier;
+	private DatabaseConnection Connect = new DatabaseConnection();
+
 	
 /*--- Constructor ---*/
 	public Dossier() {
-		ajouterDossier(num++, date);
+		this.AjouteDossier();
+		Dossier.numDossier++;
 	}
-	
+
 /*--- Getters and Setters ---*/
-	
+
+	public long getNumDossier() {
+		return numDossier;
+	}
+	public void setNumDossier(int numDossier) {
+		Dossier.numDossier = numDossier;
+	}
+	public Date getDateCreationDossier() {
+		return dateCreationDossier;
+	}
+	public void setDateCreationDossier(Date dateCreationDossier) {
+		this.dateCreationDossier = dateCreationDossier;
+	}
 
 /*--- Methods ---*/
-
 	
+	public void AjouteDossier() {
+		try {
+			String insertQuery="insert into dossier values(?,?)";
+		
+			PreparedStatement preparedStmt = Connect.getConnection().prepareStatement(insertQuery);
+			
+			dateCreationDossier=System.currentTimeMillis();
+			
+			preparedStmt.setLong(1, Dossier.numDossier);
+			preparedStmt.setDate(2,dateCreationDossier);
+			
+			preparedStmt.execute();
+			Connect.closeConnection();
+		}
+		catch(SQLException e) {
+			System.out.println(e.getMessage());
+		}
+		 
+	}
 	
+	public void deletDossier() {
+		try {
+			String deleteQuery="delete from dossier where num_dossier=?";
+			PreparedStatement preparedStmt = Connect.getConnection().prepareStatement(deleteQuery);
+			
+			preparedStmt.setLong(1, Dossier.numDossier);
+			
+			preparedStmt.execute();
+			Connect.closeConnection();
+		}
+		catch(SQLException e) {
+			System.out.println(e.getMessage());
+		}
+	}
 
 }
--- src/model/RDV.java
+++ src/model/RDV.java
@@ -1,15 +1,18 @@
 package model;
 import java.sql.Date;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
 
 public class RDV {
 	
 /*--- Properties ---*/
 	private int numRDV;
 	private Date dateRDV;
-	private float heureRDV;
+	private Date heureRDV;
+	private DatabaseConnection Connect = new DatabaseConnection();
 	
 /*--- Constructor ---*/
-	public RDV(int numRDV,Date dateRDV,float heureRDV) {
+	public RDV(int numRDV,Date dateRDV,Date heureRDV) {
 		this.numRDV=numRDV;
 		this.dateRDV=dateRDV;
 		this.heureRDV=heureRDV;
@@ -29,18 +32,87 @@ public class RDV {
 	public void setDateRDV(Date dateRDV) {
 		this.dateRDV = dateRDV;
 	}
-	public float getHeureRDV() {
+	public Date getHeureRDV() {
 		return heureRDV;
 	}
-	public void setHeureRDV(int heureRDV) {
+	public void setHeureRDV(Date heureRDV) {
 		this.heureRDV = heureRDV;
 	}
 
+	/*--- methods ---*/
 	
+	//Ajoute RDV
+	
+	public void ajouteRDV() {
+		try {
+			String insertQuery = 
+					"insert into rdv(num_rdv,date_rdv,heur_rdv) values (?,?,?)";
+			
+			PreparedStatement preparedStmt = Connect.getConnection().prepareStatement(insertQuery);
+			
+			preparedStmt.setInt(1,numRDV);
+			preparedStmt.setDate(2,dateRDV);
+			preparedStmt.setDate(3,heureRDV);
+			
+			preparedStmt.execute();
+			Connect.closeConnection();
+				
+		}
+			catch(SQLException e) {
+					System.out.println(e.getMessage());
+			}
+	}
+	
+	// Annul RDV
+	
+	public void annuleRDV() {
+		try {
+		String anuuleQuery= "delete from rdv where num_rdv=?";
+		
+		
+			PreparedStatement preparedStmt = Connect.getConnection().prepareStatement(anuuleQuery);
+			
+			preparedStmt.setInt(1,numRDV);
+			preparedStmt.execute();
+			Connect.closeConnection();
+			
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	
+	//Modifier RDV
+	
+	public void modifieRDV() {
+		try {
+			String updateQuery =
+					"UPDATE rdv SET "
+					+ "num_rdv= ?,"
+					+ "date_rdv= ?,"
+					+ "heure_rdv= ?"
+					+ "where num_rdv=?";
+			
+			PreparedStatement preparedStmt = Connect.getConnection().prepareStatement(updateQuery);
+			
+			preparedStmt.setInt(1,numRDV);
+			preparedStmt.setDate(2, dateRDV);
+			preparedStmt.setDate(3, heureRDV);
+			
+			preparedStmt.setInt(4,numRDV);
+			
+			preparedStmt.execute();
+			Connect.closeConnection();
+			
+		} catch (Exception e) {
+			System.out.println(e.getMessage());
+		}
+	}
+	
+	
+
 	
 	/*
 	public void imprimiRDV(RDV r) {
-	
-	}*/
 
 }
